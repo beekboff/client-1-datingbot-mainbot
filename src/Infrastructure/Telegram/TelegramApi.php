1dce4c980939e0cfae43b8952a128aea0195e4fc
Dmitriy Tsvetkov <beekboff@gmail.com>
2026-01-03 11:35:07 +0100

    upd

--- src/Infrastructure/Telegram/TelegramApi.php
+++ src/Infrastructure/Telegram/TelegramApi.php
@@ -121,7 +121,7 @@ final class TelegramApi
             if (isset($resp) && $resp->getStatusCode() === 403) {
                 return;
             }
-            $this->logger->error('Telegram API exception on {method}: {error}; {payload}', ['method' => $method, 'error' => $e->getMessage(), 'payload' => $payload, 'all' => $e]);
+            $this->logger->error('Telegram API exception on {method}: {error}; {payload}', ['method' => $method, 'error' => $e->getMessage(), 'payload' => $payload, 'code' => $e->getCode()]);
         }
     }
 }
